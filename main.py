a3ddd2a98586e2a463fbbbe4c1d6824fc8cce172
Danilo Ceccarelli Sano <daniloceccarelli14@gmail.com>
2022-03-08 20:12:29 -0300

    retirada dos comentarios para o desenvolvimento

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ cv2.CascadeClassifier
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
 arqCasc1 = 'br.xml'
-faceCascade1 = cv2.CascadeClassifier(arqCasc1) #classificador para o rosto
+faceCascade1 = cv2.CascadeClassifier(arqCasc1)
 hImg = 1080
 wImg = 1920
-webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)  #instancia o uso da webcam
+webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW) 
 
 
 while True:
-    s, imagem = webcam.read() #pega efeticamente a imagem da webcam
+    s, imagem = webcam.read() 
     
-    #Aqui ele só pega as placas, ta com nome de face mas é pq eu so reutilizei o codigo do reconhecimento facial
     faces = faceCascade1.detectMultiScale(
         imagem,
         minNeighbors=20,
@@ -22,25 +21,24 @@ while True:
         
     )
 
-    #Aqui eu reaproveite que toda vez que ele acha um quadrado da placa ele le, pra n ficar lendo a toa toda hora
     for (x, y, w, h) in faces:
-        cv2.rectangle(imagem, (x, y), (x+w, y+h), (0, 255, 0), 4) #escreve o retangulo em volta da placa
-        boximg = pytesseract.image_to_boxes(imagem) #divide e lê os caracteres que ele achou na imagem
+        cv2.rectangle(imagem, (x, y), (x+w, y+h), (0, 255, 0), 4)
+        boximg = pytesseract.image_to_boxes(imagem)
         Text = ""
-        for b in boximg.splitlines(): #for pra percorrer todas as letras que ele achou e vai sair desenhando e escrevendo tudo
+        for b in boximg.splitlines():
             b = b.split(' ')
             x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
             Text += b[0]
-            cv2.rectangle(imagem, (x, y), (x+w, y+h), (0, 255, 0), 4) #coloca um retangulo em volta das letras
-            cv2.putText(imagem, b[0], (x, hImg - y + 13), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (50, 205, 50), 1)#aparece as letras na imagem da camera
+            cv2.rectangle(imagem, (x, y), (x+w, y+h), (0, 255, 0), 4)
+            cv2.putText(imagem, b[0], (x, hImg - y + 13), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (50, 205, 50), 1)
         print(Text)
     
 
-    cv2.imshow('Video', imagem) #mostra a imagem captura na janela
+    cv2.imshow('Video', imagem) 
 
-    #o trecho seguinte e apenas para parar o codigo e fechar a janela
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-webcam.release() #dispensa o uso da webcam
-cv2.destroyAllWindows() #fecha todas a janelas abertas
+webcam.release()
+cv2.destroyAllWindows()
