d0745b34cb9e54dcfb99c4f75fa0bdcc4e66d94d
Danilo Ceccarelli Sano <daniloceccarelli14@gmail.com>
2022-03-23 11:17:10 -0300

    Teste percorrendo imagens dentro do diretório local - Taxa de acerto 2% dentro de 200 imagens

--- testeAcerto.py
+++ testeAcerto.py
@@ -1,47 +1,56 @@
 import cv2
 import pytesseract
 import os
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
-
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 placasXml = 'br.xml'
 placasCascade = cv2.CascadeClassifier(placasXml)
 
-# path = "images"
-# dirs = os.listdir( path )
+path = "images"
+dirs = os.listdir( path )
 
-Text = ""
-imagemInput = cv2.imread('placaSimples.jpg')
-hImg, wImg, _ = imagemInput.shape
+acertos = 0
+erros = 0
+cont = 0
 
-faces = placasCascade.detectMultiScale(
-        imagemInput,
-        minNeighbors=20,
-        minSize=(30, 30),
-	    maxSize=(300,300)
-    )
+for item in dirs:
+    cont += 1
+    Text = ""
+    imagemInput = cv2.imread("images/"+item)
+    nomeArquivoAtual = item.replace('.jpg', '')
+    nomeArquivoAtual = nomeArquivoAtual.replace('.JPG', '')
+    nomeArquivoAtual = nomeArquivoAtual.replace('-', '')
 
-for (x, y, w, h) in faces:
+    hImg, wImg, _ = imagemInput.shape
 
-    cv2.rectangle(imagemInput, (x, y), (x+w, y+h), (0, 255, 0), 4)
-    boximg = pytesseract.image_to_boxes(imagemInput)
-    
-    for b in boximg.splitlines():
-        b = b.split(' ')
-        x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
-        Text += b[0]
-        cv2.rectangle(imagemInput, (x, hImg - y), (w, wImg - h), (50, 50, 255), 1)
-        
-print(Text)
+    faces = placasCascade.detectMultiScale(
+            imagemInput,
+            minNeighbors=20,
+            minSize=(30, 30),
+            maxSize=(300,300)
+        )
 
-# acertos = 0
-# erros = 0
-# # cont = 0
-# contText = str(cont)
-# acertosText = str(acertos)
-# errosText = str(erros)
-# print("Total de arquivos percorridos:" + contText + "\nTotal de acertos: " + acertosText +"\nTotal de erros: " + errosText)
+    for (x, y, w, h) in faces:
+        boximg = pytesseract.image_to_boxes(imagemInput)
+        for b in boximg.splitlines():
+            b = b.split(' ')
+            x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+            Text += b[0]
+            
+    Text = Text.replace('~', '')
+    Text = Text.replace('-', '')
+    Text = Text.replace(':', '')
+    if(Text.find(nomeArquivoAtual) > -1):
+        acertos += 1
+    else:
+        erros += 1
 
-cv2.imshow('Detected text', imagemInput)
-cv2.waitKey(0)
+TaxaAcerto = acertos/cont*100
+TaxaErro = erros/cont*100
+
+contText = str(cont)
+TaxaAcerto = str(TaxaAcerto)
+TaxaErro = str(TaxaErro)
+print("Total de arquivos percorridos:" + contText + "\nTotal de acertos: " + TaxaAcerto +"%\nTotal de erros: " + TaxaErro+"%")
+
 
--- testeDoArttigo.py
+++
@@ -1,8 +0,0 @@
-import os, cv2, mahotas, pytesseract, time
-from PIL import Image
-
-
-
-img = cv2.cvtColor(imagemInput, cv2.COLOR_BGR2GRAY)
-suave = cv2.GaussianBlur(img, (3, 3), 10)
-T = mahotas.thresholding.otsu(suave)
