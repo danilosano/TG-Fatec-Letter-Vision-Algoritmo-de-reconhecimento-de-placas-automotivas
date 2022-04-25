626e83c5c44a648890c6c14d21f55e3c46f6b08a
Danilo Ceccarelli Sano <daniloceccarelli14@gmail.com>
2022-04-25 15:28:19 -0300

    Testando o algoritmo canny

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from matplotlib import pyplot as plt
-import cv2
+import cv2 , pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
 img = cv2.imread('imagemSimples.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #converte P&B
-cv2.imshow("Imagem P&B", img)
-#Função calcHist para calcular o hisograma da imagem
-h_eq = cv2.equalizeHist(img)
+blurred = cv2.GaussianBlur(img, (5,5), 0)
+mid = cv2.Canny(blurred, 105, 140)
 
-h = cv2.calcHist([h_eq], [0], None, [256], [0, 256])
-plt.figure()
-plt.title("Histograma P&B")
-plt.xlabel("Intensidade")
-plt.ylabel("Qtde de Pixels")
-plt.plot(h)
-plt.xlim([0, 256])
-plt.show()
+cv2.imshow("Imagem P&B", mid)
 cv2.waitKey(0)
